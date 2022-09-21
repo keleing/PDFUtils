26a2d341b00320cd31a44b29ab903c00426e707a
liwei <liwei@example.com>
2022-09-21 14:57:21 +0800

    新增提取pdf缩略图

--- src/main/java/com/example/demo/DocumentUtils.java
+++ src/main/java/com/example/demo/DocumentUtils.java
@@ -1,31 +1,27 @@
 package com.example.demo;
 
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
+import com.itextpdf.text.*;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.pdf.parser.*;
-//import gz.gdtel.com.ParterOperationSystem.pojo.Document;
-//import gz.gdtel.com.ParterOperationSystem.pojo.DocumentDir;
-//import gz.gdtel.com.ParterOperationSystem.pojo.DocumentShow;
-//import gz.gdtel.com.ParterOperationSystem.pojo.DocumentVo;
-//import gz.gdtel.com.ParterOperationSystem.utils.documentUtil.CustomRenderListener;
-//import gz.gdtel.com.ParterOperationSystem.utils.documentUtil.MatchItem;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Image;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.awt.image.RenderedImage;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 文件处理工具类
@@ -33,30 +29,33 @@ import java.util.List;
 public class DocumentUtils {
     //文件压缩比例
     public static float FACTOR = 0.5f;
+
     public DocumentUtils() {
     }
 
     /**
      * 文档转PDF
+     *
      * @param source 源文件路径
      * @param target 目标文件目录
      * @return
      */
-    public int beginToPdf(String source, String target){
+    public int beginToPdf(String source, String target) {
         String command;
-        int exitStatus=0;
+        int exitStatus = 0;
         String osName = System.getProperty("os.name");
         if (osName.contains("Windows")) {
             command = "cmd /c start soffice --headless --invisible --convert-to pdf:writer_pdf_Export " + source + " --outdir " + target;
         } else {
-            command = "/opt/libreoffice7.3/program/soffice --headless --invisible --convert-to pdf:writer_pdf_Export "+ source + " --outdir " + target;
+            command = "/opt/libreoffice7.3/program/soffice --headless --invisible --convert-to pdf:writer_pdf_Export " + source + " --outdir " + target;
         }
         //command = "/opt/libreoffice7.3/program/soffice --headless --invisible --convert-to pdf /var/www/html/dist/index.html --outdir /var/lib/tomcat9/webapps/home";
         System.out.println(command);
         exitStatus = executeOSCommand(command);
-        System.out.println("-----"+exitStatus);
+        System.out.println("-----" + exitStatus);
         return exitStatus;
     }
+
     /**
      * 调用操作系统的控制台，执行 command 指令
      * 执行该方法时，并没有等到指令执行完毕才返回，而是执行之后立即返回，返回结果为 0，只能说明正确的调用了操作系统的控制台指令，但执行结果如何，是否有异常，在这里是不能体现的，所以，更好的姿势是用同步转换功能。
@@ -69,8 +68,8 @@ public class DocumentUtils {
         List<String> errorList = new ArrayList<>();
         try {
             process = Runtime.getRuntime().exec(command); // 转换需要时间，比如一个 3M 左右的文档大概需要 8 秒左右，但实际测试时，并不会等转换结束才执行下一行代码，而是把执行指令发送出去后就立即执行下一行代码了。
-            ThreadUtil stdotThread = new ThreadUtil(stdotList,process.getInputStream());
-            ThreadUtil errorThread = new ThreadUtil(errorList,process.getErrorStream());
+            ThreadUtil stdotThread = new ThreadUtil(stdotList, process.getInputStream());
+            ThreadUtil errorThread = new ThreadUtil(errorList, process.getErrorStream());
             stdotThread.start();
             errorThread.start();
 
@@ -81,13 +80,13 @@ public class DocumentUtils {
         int exitStatus = 0;
         try {
             exitStatus = process.waitFor();
-            System.out.println("进程结束："+exitStatus);
+            System.out.println("进程结束：" + exitStatus);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
         if (exitStatus == 0) {
-            System.out.println("退出值："+process.exitValue());
+            System.out.println("退出值：" + process.exitValue());
             exitStatus = process.exitValue();
         }
         // 销毁子进程
@@ -171,8 +170,8 @@ public class DocumentUtils {
                 under.endText();
             }
             //判断是否有生成水印文件
-            File file1 =new File(outputFile);
-            System.out.println("添加水印成功！文件是否存在："+file1.exists());
+            File file1 = new File(outputFile);
+            System.out.println("添加水印成功！文件是否存在：" + file1.exists());
             return true;
         } catch (IOException e) {
             System.out.println("添加水印失败！错误信息为: " + e);
@@ -250,7 +249,7 @@ public class DocumentUtils {
      * @param pdfReader pdf处理器
      * @param keyWords  关键字
      * @param pageNum 关键字所在页码
-     * @return  返回X\Y坐标匹配类
+     * @return 返回X\Y坐标匹配类
      * @throws IOException
      */
 //    public MatchItem getKeyWordLocation(PdfReader pdfReader, String keyWords, int pageNum) throws IOException {
@@ -292,6 +291,7 @@ public class DocumentUtils {
 
     /**
      * 凸显关键字
+     *
      * @param pdfReader
      * @param page      页码
      * @param x         关键字 X坐标
@@ -321,7 +321,8 @@ public class DocumentUtils {
 
     /**
      * PDF文件压缩
-     * @param src 源文件路径，包括文件名
+     *
+     * @param src  源文件路径，包括文件名
      * @param dest 目标文件路径，包括文件名
      */
     public void compressPdf(String src, String dest) {
@@ -330,7 +331,7 @@ public class DocumentUtils {
         //System.out.println("获取的文件路径:"+src);
         try {
             PdfReader reader = new PdfReader(src);
-            System.out.println("文件读取成功"+src);
+            System.out.println("文件读取成功" + src);
             //获取文件密码
             Field f = PdfReader.class.getDeclaredField("ownerPasswordUsed");
             f.setAccessible(true);
@@ -355,11 +356,11 @@ public class DocumentUtils {
                     int width = (int) (bi.getWidth() * FACTOR);
                     int height = (int) (bi.getHeight() * FACTOR);
                     //图片
-                    if (width<=0){
-                        width=1;
+                    if (width <= 0) {
+                        width = 1;
                     }
-                    if (height<=0){
-                        height=1;
+                    if (height <= 0) {
+                        height = 1;
                     }
                     BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
                     //坐标变化
@@ -370,7 +371,7 @@ public class DocumentUtils {
                     ByteArrayOutputStream imgBytes = new ByteArrayOutputStream();
                     //判断文件流的大小，超过500k的才进行压缩，否则不进行压缩
                     //System.out.println("++++++++++++:"+img.getData().getDataBuffer().getSize());
-                    if(img.getData().getDataBuffer().getSize()>512000){
+                    if (img.getData().getDataBuffer().getSize() > 512000) {
                         System.out.println("正在压缩-------");
                         ImageIO.write(img, "WEBP", imgBytes);
                         stream.clear();
@@ -384,7 +385,7 @@ public class DocumentUtils {
                         stream.put(PdfName.BITSPERCOMPONENT, new PdfNumber(8));
                         stream.put(PdfName.COLORSPACE, PdfName.DEFAULTRGB);
                         System.out.println("压缩完成-------");
-                    }else {
+                    } else {
                         ImageIO.write(img, "JPG", imgBytes);
                     }
                 }
@@ -406,11 +407,12 @@ public class DocumentUtils {
 
     /**
      * 获取路径下的所有文件/文件夹
-     * @param directoryPath 需要遍历的文件夹路径
+     *
+     * @param directoryPath  需要遍历的文件夹路径
      * @param isAddDirectory 是否将子文件夹的路径也添加到list集合中
      * @return
      */
-    public  List<String> getAllFile(String directoryPath,boolean isAddDirectory) {
+    public List<String> getAllFile(String directoryPath, boolean isAddDirectory) {
         List<String> list = new ArrayList<String>();
         File baseFile = new File(directoryPath);
         if (baseFile.isFile() || !baseFile.exists()) {
@@ -419,16 +421,102 @@ public class DocumentUtils {
         File[] files = baseFile.listFiles();
         for (File file : files) {
             if (file.isDirectory()) {
-                if(isAddDirectory){
+                if (isAddDirectory) {
                     list.add(file.getAbsolutePath());
                 }
-                list.addAll(getAllFile(file.getAbsolutePath(),isAddDirectory));
+                list.addAll(getAllFile(file.getAbsolutePath(), isAddDirectory));
             } else {
                 list.add(file.getAbsolutePath());
             }
         }
         return list;
     }
+
+    /**
+     * 抽取PDF文件
+     *
+     * @param sourceFile  源PDF文件路径
+     * @param targetFile  目标PDF文件路径
+     * @param fromPageNum 起始页码
+     * @param toPageNum   结束页码
+     */
+    public static void extract(String sourceFile, String targetFile, int fromPageNum, int toPageNum) {
+        Objects.requireNonNull(sourceFile);
+        Objects.requireNonNull(targetFile);
+        PdfReader reader = null;
+        Document document = null;
+        FileOutputStream outputStream = null;
+        try {
+            // 读取源文件
+            reader = new PdfReader(sourceFile);
+            // 创建新的文档
+            document = new Document();
+            // 创建目标PDF文件
+            outputStream = new FileOutputStream(targetFile);
+            PdfCopy pdfCopy = new PdfSmartCopy(document, outputStream);
+            // 获取源文件的页数
+            int pages = reader.getNumberOfPages();
+            document.open();
+
+            // 注意此处的页码是从1开始
+            for (int page = 1; page <= pages; page++) {
+                if (page >= fromPageNum && page <= toPageNum) {
+                    pdfCopy.addPage(pdfCopy.getImportedPage(reader, page));
+                }
+            }
+        } catch (IOException | DocumentException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (document != null) {
+                document.close();
+            }
+
+            if (outputStream != null) {
+                try {
+                    outputStream.flush();
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void pdfToImage(String src, String dest) {
+
+        try {
+            PdfReader reader = new PdfReader(src);
+            System.out.println("文件读取成功");
+            //获取文件密码
+            Field f = PdfReader.class.getDeclaredField("ownerPasswordUsed");
+            f.setAccessible(true);
+            f.set(reader, Boolean.TRUE);
+            // 获取外部参照对象数量
+            int n = reader.getXrefSize();
+            PdfObject object;
+            object=reader.getPdfObject(1);
+            PRStream stream= (PRStream)object;
+            //stream.setData(reader.getPageContent(1));
+            // Look for image and manipulate image stream
+            PdfImageObject image = new PdfImageObject(stream);
+            BufferedImage img = image.getBufferedImage();
+            File outFile= new File(dest);
+            ImageIO.write(img,"jpg",outFile);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+    }
 }
-
-
--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -10,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -23,7 +27,7 @@ class Demo1ApplicationTests {
     DocumentUtils documentUtils =new DocumentUtils();
     @Test
     void renFile(){
-        String sour ="C:\\Users\\liwei\\Desktop\\3、其他政务\\";
+        String sour ="C:\\Users\\liwei\\Desktop\\6、卫健\\";
         //String sour ="C:\\Users\\liwei\\Desktop\\文件重命名\\";
         List<String> paths = documentUtils.getAllFile(sour,false);
         Map<String ,String> nameMap= new HashMap<>();
@@ -115,7 +119,35 @@ class Demo1ApplicationTests {
 
     @Test
     void insertTable(){
-        documentUtils.compressPdf("C:\\Users\\liwei\\Desktop\\40837370-a27e-49cf-bfe7-6705698c0a12.pdf","C:\\Users\\liwei\\Desktop\\compress_40837370-a27e-49cf-bfe7-6705698c0a12.pdf");
+        String source = "C:\\Users\\liwei\\Desktop\\全部解压文件\\";
+        String dirPath = "C:\\Users\\liwei\\Desktop\\image\\";
+        File dir = new File(source);
+        if (!dir.exists() || !dir.isDirectory()) {// 判断是否存在目录
+            System.out.println("目录不存在");
+        }
+        File dir2 = new File(dirPath);
+        if (!dir2.exists()) {// 判断是否存在目录
+            dir2.mkdir();
+        }
+        String[] files = dir.list();
+        for (int i = 0; i < files.length; i++) {
+            System.out.println(files[i]);
+            try {
+                File file =new File(source+files[i]);
+                PDDocument pdDocument= PDDocument.load(file);
+                PDFRenderer renderer = new PDFRenderer(pdDocument);
+                int pageCount = pdDocument.getNumberOfPages();
+                BufferedImage image = renderer.renderImageWithDPI(0, 50);
+//          BufferedImage image = renderer.renderImage(i, 2.5f);
+                String imageName = files[i].substring(files[i].lastIndexOf("_")+1,files[i].lastIndexOf("."))+".png";
+                System.out.println(imageName);
+                File des =new File(dirPath+imageName);
+                ImageIO.write(image, "PNG",des );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
